1553dbf2a253d290001a6f57fd9f6ccee2d99462
Wolvinny <treblecorgi@gmail.com>
2022-06-13 22:27:03 +0200

    added ReasonKey enummy

---
+++ src/main/java/com/luwuna/ravyapi/enums/ReasonKey.java
@@ -0,0 +1,15 @@
+package com.luwuna.ravyapi.enums;
+
+public enum ReasonKey {
+    DM_ADS,
+    HARASSMENT,
+    PHISHING,
+    IMPERSONATION,
+    FRAUD,
+    RAIDING,
+    THREATS,
+    NSFW,
+    DATA_EXPIRATION,
+    SLANDER,
+    OTHER
+}
